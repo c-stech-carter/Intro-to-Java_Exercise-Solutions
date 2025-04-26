2678749d2d5ca4ed48b06259c145c1c23d56360b
c-stech-carter <punkynyrd@gmail.com>
2025-04-26 17:17:20 -0600

    Add Exercise01_01.java
    Exercise 1-1 Solution

---
+++ Chapter_01/Exercise01_01.java
@@ -0,0 +1,11 @@
+/*
+Solution for Exercise 1-1 from Intro to Java 13th Edition
+*/
+
+public class Exercise01_01 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
