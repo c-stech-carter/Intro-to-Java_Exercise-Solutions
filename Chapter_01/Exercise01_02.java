45e8c748c932b83cc37cda356d4677fa4700d429
c-stech-carter <punkynyrd@gmail.com>
2025-04-26 17:20:20 -0600

    Add Exercise01_02.java

---
+++ Chapter_01/Exercise01_02.java
@@ -0,0 +1,13 @@
+/*
+Solution for Exercise 1-2 from Intro to Java 13th Edition
+*/
+
+public class Exercise01_02 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
