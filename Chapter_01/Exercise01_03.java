6f360b43b0db937de33edd42ad52e1626d275f9c
c-stech-carter <punkynyrd@gmail.com>
2025-04-26 17:28:26 -0600

    Add Exercise01_03.java
    Solution for Exercise 1-3 from Intro to Java 13th Edition

---
+++ Chapter_01/Exercise01_03.java
@@ -0,0 +1,22 @@
+/*
+Solution for Exercise 1-2 from Intro to Java 13th Edition
+*/
+
+public class Exercise01_03 {
+    public static void main(String[] args) {
+        System.out.print("    J     A     V     V    A     \n" +
+                         "    J    A A     V   V    A A    \n" +
+                         "J   J   AAAAA     V V    AAAAA   \n" +
+                         " J J   A     A     V    A     A  \n");
+    }
+}
+
+/*
+Alternatively, switch with multiple println statements:
+
+System.out.println("    J     A     V     V    A     ");
+System.out.println("    J    A A     V   V    A A    ");
+System.out.println("J   J   AAAAA     V V    AAAAA   ");
+System.out.println(" J J   A     A     V    A     A  ");
+
+*/
