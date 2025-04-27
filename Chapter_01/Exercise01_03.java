7348bebaec16962e9b9925ac56721e1f36ee9a99
c-stech-carter <punkynyrd@gmail.com>
2025-04-26 20:55:38 -0600

    Update Exercise01_03.java

--- Chapter_01/Exercise01_03.java
+++ Chapter_01/Exercise01_03.java
@@ -1,5 +1,13 @@
 /*
 Solution for Exercise 1-2 from Intro to Java 13th Edition
+
+*1.3 (Display a pattern) Write a program that displays the following pattern:
+
+    J     A     V     V    A     
+    J    A A     V   V    A A    
+J   J   AAAAA     V V    AAAAA   
+ J J   A     A     V    A     A
+ 
 */
 
 public class Exercise01_03 {
