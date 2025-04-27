bf1d7dffd5e38ac24fc4aededb790a4194ec8fc1
c-stech-carter <punkynyrd@gmail.com>
2025-04-26 21:01:37 -0600

    Update Exercise01_07.java

--- Chapter_01/Exercise01_07.java
+++ Chapter_01/Exercise01_07.java
@@ -2,6 +2,20 @@
 Solution for Exercise 1-7
 
 The online check exercises tool is not technically correct for grading this exercise.
+
+
+1.7 (Approximate pi) 
+pi can be computed using the following formula:
+
+pi = 4 × (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + ...)
+
+Write a program that displays the result of:
+4 × (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11)
+and
+4 × (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + 1/13 - 1/15).
+
+Use 1.0 instead of 1 in your program.
+
 */
 
 
