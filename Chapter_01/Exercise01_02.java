d3c58f8ad1ea40a9dac05412bdba8ba6533e63bb
c-stech-carter <punkynyrd@gmail.com>
2025-04-26 20:54:19 -0600

    Update Exercise01_02.java

--- Chapter_01/Exercise01_02.java
+++ Chapter_01/Exercise01_02.java
@@ -1,5 +1,7 @@
 /*
 Solution for Exercise 1-2 from Intro to Java 13th Edition
+
+1.2 (Display five messages) Write a program that displays 'Welcome to Java' five times.
 */
 
 public class Exercise01_02 {
