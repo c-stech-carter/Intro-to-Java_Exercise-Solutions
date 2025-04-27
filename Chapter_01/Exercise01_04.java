ef8b5c5428eeaf6297aa8329575ccfcfad2cf90b
c-stech-carter <punkynyrd@gmail.com>
2025-04-26 18:07:46 -0600

    Add Exercise01_04.java
    Solution for Exercise 1-4

---
+++ Chapter_01/Exercise01_04.java
@@ -0,0 +1,15 @@
+/*
+Solution for Exercise 1-4 from Intro to Java 13th Edition
+
+(The online automatic check exercise does tool does not work correctly for this assignment)
+*/
+
+public class Exercise01_04 {
+    public static void main(String[] args) {
+        System.out.println("a      a^2    a^3");
+        System.out.println("1      1      1");
+        System.out.println("2      4      8");
+        System.out.println("3      9      27");
+        System.out.println("4      16     64");
+    }
+}
