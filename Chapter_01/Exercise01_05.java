373877c76756d84b4dcb857c3b8d6430ceb0b823
c-stech-carter <punkynyrd@gmail.com>
2025-04-26 20:58:30 -0600

    Update Exercise01_05.java

--- Chapter_01/Exercise01_05.java
+++ Chapter_01/Exercise01_05.java
@@ -1,5 +1,8 @@
 /*
 Solution for Exercise 1-5 from Intro to Java 13th Edition
+
+
+1.5 (Compute expressions) Write a program that displays the result of (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5)
 */
 
 public class Exercise01_05 {
