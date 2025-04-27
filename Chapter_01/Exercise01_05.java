14465b77ca0a8c70c322492f7c3e55b46f8a9f3f
c-stech-carter <punkynyrd@gmail.com>
2025-04-26 18:13:59 -0600

    Create Exercise01_05.java
    Solution for Exercise 1-5

---
+++ Chapter_01/Exercise01_05.java
@@ -0,0 +1,9 @@
+/*
+Solution for Exercise 1-5 from Intro to Java 13th Edition
+*/
+
+public class Exercise01_05 {
+    public static void main(String[] args) {
+        System.out.print((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+    }
+}
