125208130884e476a5fd5a154500560c9225f241
c-stech-carter <punkynyrd@gmail.com>
2025-04-26 20:56:33 -0600

    Update Exercise01_04.java

--- Chapter_01/Exercise01_04.java
+++ Chapter_01/Exercise01_04.java
@@ -2,6 +2,14 @@
 Solution for Exercise 1-4 from Intro to Java 13th Edition
 
 (The online automatic check exercise does tool does not work correctly for this assignment)
+
+1.4 (Print a table) Write a program that displays the following table:
+a      a^2    a^3
+1      1      1
+2      4      8
+3      9      27
+4      16     64
+
 */
 
 public class Exercise01_04 {
